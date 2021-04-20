f22edb6b291c9bc6c09e8cf9150822d8838ca271
Velythyl <charlie.gauthier@umontreal.ca>
2021-04-19 23:26:15 -0400

    f' g'

--- social_rl/adversarial_env/domain_transfer_experiments.py
+++ social_rl/adversarial_env/domain_transfer_experiments.py
@@ -7,19 +7,38 @@ import gym
 import tensorflow as tf
 import numpy as np
 from tf_agents.policies import policy_loader
-
+from social_rl import gym_multigrid
+import matplotlib.pyplot as plt
+from tf_agents.trajectories import time_step as ts_lib
+tf.compat.v1.enable_v2_behavior()
 #source_shift = []
 #target_shift = []
+from social_rl.adversarial_env import adversarial_env
 
-all_source_agents = ["/home/charlie/SDRIVE/datasets/any_shifts/paired/policy_saved_model/agent/0/policy_000499950"]
+all_source_agents = ["/home/charlie/SDRIVE/datasets/no_shift/policy_saved_model/agent/0/policy_000499950"]
 all_variable_agents = []
 
 all_source_agents = [tf.compat.v2.saved_model.load(a) for a in all_source_agents]
 all_variable_agents = [tf.compat.v2.saved_model.load(a) for a in all_variable_agents]
 
 sequences = [
-  []
-
+[
+    12,
+    0,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25
+  ] # corridor
 ]
 
 def get_envs():
@@ -28,35 +47,63 @@ def get_envs():
   sources = []
 
   for i, sequence in enumerate(sequences):
-    for shift in [source_shift, target_shift]:
-      env = gym.make("MultiGrid-Adversarial-v0")
-      #todo apply shift
-      #targets.append(TODO)
-      #sources.append(TODO)
-      targets.append(env)
-      sources.append(env)
+    #for shift in [source_shift, target_shift]:
+    py_env = adversarial_env.load("MultiGrid-Adversarial-v0")
+    tf_env = adversarial_env.AdversarialTFPyEnvironment(py_env)
+
+    done = False
+    for s in sequence:
+      _, _, done, _ = tf_env.step_adversary(s)
+
+#    plt.imshow(tf_env.render('rgb_array'))
+   # plt.show()
+    while not done:
+      _, _, done, _ = tf_env.step_adversary(s)
+
+
+
+    plt.imshow(py_env.render())
+    plt.show()
+
+    #todo apply shift
+    #targets.append(TODO)
+    #sources.append(TODO)
+    targets.append(tf_env)
+    sources.append(tf_env)
     names.append(i)
 
-  return sources, targets, names
+  return zip(sources, targets, names)
 
 def load_agent(checkpoint_path):
   agent = tf.compat.v2.saved_model.load(checkpoint_path)
   return agent
 
+def backwards_compatible_timestep(new_ts):
+  """Remove new observations added in later versions of the environment."""
+  old_obs = {
+      'image': new_ts.observation['image'],
+      'time_step': new_ts.observation['time_step']
+  }
+  return ts_lib.TimeStep(
+      new_ts.step_type,
+      new_ts.reward,
+      new_ts.discount,
+      old_obs)
+
 def run_agent_on_env(agent, env):
   done = False
   rewards = []
 
-  obs = env.reset_agent()
+  timestep = env.reset()
   policy_state = agent.get_initial_state(1)
 
   while not done:
-    policy_step = agent.action(obs, policy_state=policy_state)
+    policy_step = agent.action(timestep, policy_state=policy_state)
 
     policy_state = policy_step.state
 
-    obs, reward, done, _ = env.step(policy_step.action)
-    rewards.append(reward)
+    timestep = env.step(policy_step.action)
+    #rewards.append(reward)
   return np.array(rewards)
 
 by_env = {}
